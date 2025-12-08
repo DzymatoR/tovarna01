3b3882b21defb6b86d7e3c57834cd3832ca5a584
Jakub Šmíd <dzymator@gmail.com>
2025-12-08 14:19:08 +0100

    sync

---
+++ Devices/VP_counter_01/VP_citac/.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
+ C:/Users/j.smid.KORESCZ/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ C:/Users/j.smid.KORESCZ/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
---
+++ Devices/VP_counter_01/VP_citac/.pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.d
@@ -0,0 +1,3 @@
+.pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.o: \
+ C:/Users/j.smid.KORESCZ/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
+ C:/Users/j.smid.KORESCZ/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
